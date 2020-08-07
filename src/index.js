2939e3cabdfcb711e99ea1bc2a03c20d9739bbd8
Martin Cervantes <cervantes.martine@gmail.com>
2020-08-06 19:52:14 -0600

    Add event listener to hide and show menu or contact us

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import _ from 'lodash';
-import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+import './css/reset.css';
+import './css/style.css';
+import './css/header.css';
+import './css/content.css';
+import './css/footer.css';
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+import header from './js/header';
+import menu from './js/menu';
+import contact from './js/contact';
+import footer from './js/footer';
 
-  return element;
-}
+const content = document.getElementById('content');
 
-document.body.appendChild(component());
+document.body.insertBefore(header(), content);
+
+menu();
+
+document.body.appendChild(contact());
+
+document.body.appendChild(footer());
+
+const content2 = document.getElementById('content2');
+const link_menu = document.getElementById('menu');
+const link_contact = document.getElementById('contact');
+
+link_menu.addEventListener('click', () => {
+  content.classList.remove('none');
+  content.classList.add('grid');
+  content2.classList.add('none');
+});
+
+link_contact.addEventListener('click', () => {
+  content.classList.remove('grid');
+  content.classList.add('none');
+  content2.classList.remove('none');
+});
